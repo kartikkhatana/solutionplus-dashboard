2f41dca549ea817c51212393e2c0df421f03723c
kartikkhatana <kartikkhatana1@gmail.com>
2025-10-02 23:22:21 +0400

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -22,8 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} antialiased`}
-      // style={{ fontFamily: 'var(--font-roboto), sans-serif' }}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
